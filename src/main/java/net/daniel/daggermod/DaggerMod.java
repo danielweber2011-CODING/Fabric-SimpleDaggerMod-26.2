9c0416a79c57935b267a6daa4e0349cf1fa406eb
danielweber2011-CODING <daniel_weber2011@web.de>
2026-08-12 15:32:35 +0200

    Initial commit

---
+++ src/main/java/net/daniel/daggermod/DaggerMod.java
@@ -0,0 +1,17 @@
+package net.daniel.daggermod;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.resources.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DaggerMod implements ModInitializer {
+	public static final String MOD_ID = "daggermod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
